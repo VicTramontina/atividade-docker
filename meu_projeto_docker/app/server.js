8d9f5a2c494f2ee9e994fb0c92daeb63673196d9
Victor Tramontina <108145998+VicTramontina@users.noreply.github.com>
2025-04-15 20:50:39 -0300

    Update server.js

--- meu_projeto_docker/app/server.js
+++ meu_projeto_docker/app/server.js
@@ -1,34 +1,37 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+// Carregar variáveis de ambiente
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
+// Criando a conexão com o banco de dados MySQL
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST,      // Usando o nome do serviço "db" no Docker Compose
+  user: process.env.DB_USER,      // Usuário do banco de dados
+  password: process.env.DB_PASSWORD,  // Senha do banco de dados
+  database: process.env.DB_NAME   // Nome do banco de dados
 });
 
-connection.connect(err => {
+// Testando a conexão com o banco de dados
+connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err);
+    console.error('Erro ao conectar no banco de dados: ' + err.stack);
     return;
   }
-  console.log('Connected to the MySQL database.');
+  console.log('Conectado ao banco de dados como ID ' + connection.threadId);
 });
 
 app.get('/', (req, res) => {
-  connection.query('SELECT message FROM messages LIMIT 1', (err, results) => {
-    if (err) {
-      console.error('Error fetching message from database:', err);
-      res.status(500).send('Error fetching message from database');
-      return;
-    }
-    res.send(results[0].message);
+  connection.query('SELECT "Hello, Docker + MySQL!" AS message', (err, results) => {
+    if (err) throw err;
+    res.send(`<h1>${results[0].message}</h1>`);
   });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
